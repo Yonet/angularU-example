5f841c990158d6827928211d054795b37b5027ab
Aysegul Yonet <aysegulyonet@gmail.com>
2015-06-22 00:00:35 -0700

    onChange is not firing.

--- app.js
+++ app.js
@@ -9,12 +9,59 @@ if (typeof __decorate !== "function") __decorate = function (decorators, target,
 if (typeof __metadata !== "function") __metadata = function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
+if (typeof __param !== "function") __param = function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 /// <reference path="typings/angular2/angular2.d.ts" />
+var angular = require('angular2/angular2');
 var angular2_1 = require('angular2/angular2');
+var di_1 = require('angular2/di');
+var d3 = require('d3');
+var BarGraph = (function () {
+    function BarGraph(elementRef, width, height) {
+        var el = elementRef.domElement;
+        var graph = d3.select(el);
+        this.divs = graph.
+            append('div').
+            attr({
+            'class': 'chart'
+        }).
+            style({
+            'width': width + 'px',
+            'height': height + 'px',
+        }).
+            selectAll('div');
+    }
+    BarGraph.prototype.render = function (newValue) {
+        if (!newValue)
+            return;
+        this.divs.data(newValue).enter().append('div')
+            .transition().ease('elastic')
+            .style('width', function (d) { return d + '%'; })
+            .text(function (d) { return d + '%'; });
+    };
+    BarGraph.prototype.onChange = function (updated) {
+        console.log('data changed', updated.data);
+        this.data = updated.data.currentValue;
+        this.render(this.data);
+    };
+    BarGraph = __decorate([
+        angular2_1.Directive({
+            selector: 'bar-graph',
+            lifecycle: [angular2_1.onChange],
+            properties: ['data']
+        }),
+        __param(0, di_1.Inject(angular2_1.ElementRef)),
+        __param(1, angular2_1.Attribute('width')),
+        __param(2, angular2_1.Attribute('height')), 
+        __metadata('design:paramtypes', [angular2_1.ElementRef, String, String])
+    ], BarGraph);
+    return BarGraph;
+})();
 var AppComponent = (function () {
     function AppComponent() {
         this.name = 'Angular 2 + D3';
-        this.values = [10, 49, 29, 69, 37];
+        this.values = [10, 20, 30, 40, 60];
     }
     AppComponent.prototype.doneTyping = function ($event) {
         if ($event.which === 13) {
@@ -24,14 +71,14 @@ var AppComponent = (function () {
     };
     AppComponent = __decorate([
         angular2_1.Component({
-            selector: 'viz-app' //Defines the viz-app tag
+            selector: 'viz-app'
         }),
         angular2_1.View({
-            template: "\n\t<h1>{{name}} example</h1>\n\t<p>Values:</p>\n\t<ul>\n\t\t<li *for=\"#value of values\">\n\t\t\t{{value}}\n\t\t</li>\n\t</ul>\n\t<input #num (keyup)=\"doneTyping($event)\">\n\t",
-            directives: [angular2_1.For]
+            directives: [BarGraph],
+            template: "\n  \t<h1>{{name}} example</h1>\n\t<p>Values:</p>\n\t<ul>\n\t\t<li *for=\"#value of graphData\">\n\t\t\t{{value}}\n\t\t</li>\n\t</ul>\n\t<input #num (keyup)=\"doneTyping($event)\">\n\t<bar-graph bind-data=\"values\" width=\"1200\" height=\"500\"></bar-graph>\n\n  "
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
     return AppComponent;
 })();
-angular2_1.bootstrap(AppComponent);
+angular.bootstrap(AppComponent);
--- app.ts
+++ app.ts
@@ -1,32 +1,79 @@
 /// <reference path="typings/angular2/angular2.d.ts" />
-import {Component, View, bootstrap, For} from 'angular2/angular2';
+import * as angular from 'angular2/angular2';
+import {Component, Directive, View, Attribute, bootstrap, For, onChange, ElementRef} from 'angular2/angular2';
+import {Inject} from 'angular2/di';
+import * as d3 from 'd3';
+
+
+@Directive({
+	selector: 'bar-graph',
+	lifecycle: [onChange],
+	properties: ['data']
+})
+class BarGraph {
+	data: Array<number>;
+	divs: any;
+	constructor(
+		@Inject(ElementRef) elementRef: ElementRef,
+		@Attribute('width') width: string,
+		@Attribute('height') height: string) {
+
+		var el: any = elementRef.domElement;
+		var graph: any = d3.select(el);
+
+		this.divs = graph.
+			append('div').
+			attr({
+				'class': 'chart'
+			}).
+			style({
+				'width': width + 'px',
+				'height': height + 'px',
+			}).
+			selectAll('div');
+	}
+
+	render(newValue) {
+		if (!newValue) return;
+
+		this.divs.data(newValue).enter().append('div')
+			.transition().ease('elastic')
+			.style('width', d => d + '%')
+			.text(d => d + '%');
+
+	}
+
+	onChange(updated) {
+		console.log('data changed', updated.data);
+		this.data = updated.data.currentValue;
+		this.render(this.data);
+	}
+}
 
 @Component({
-	selector: 'viz-app' //Defines the viz-app tag
+	selector: 'viz-app'
 })
 @View({
+	directives: [BarGraph],
 	template: `
-	<h1>{{name}} example</h1>
+  	<h1>{{name}} example</h1>
 	<p>Values:</p>
 	<ul>
-		<li *for="#value of values">
+		<li *for="#value of graphData">
 			{{value}}
 		</li>
 	</ul>
 	<input #num (keyup)="doneTyping($event)">
-	`,
-	directives: [For]
-})
+	<bar-graph bind-data="values" width="1200" height="500"></bar-graph>
 
+  `
+})
 class AppComponent {
-	name: string;
-	values
+	values: Array<number>;
 	constructor() {
 		this.name = 'Angular 2 + D3';
-		this.values = [10, 49, 29, 69, 37];
-		
+		this.values = [10, 20, 30, 40, 60];
 	}
-
 
 	doneTyping($event) {
 		if ($event.which === 13) {
@@ -34,8 +81,7 @@ class AppComponent {
 			$event.target.value = "";
 		}
 	}
+
 }
 
-
-
-bootstrap(AppComponent); 
+angular.bootstrap(AppComponent);
